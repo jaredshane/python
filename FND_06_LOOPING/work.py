8e47d0e091b8a206894448db56a857873dd46a4a
Jared Fuller <jaredshane@gmail.com>
2018-04-03 11:34:59 -0500

    added the 6th section of the orientation work

---
+++ FND_06_LOOPING/work.py
@@ -0,0 +1,19 @@
+flowers = ['Lily', 'Snapdragon', 'Rose', 'Tulip']
+large_flowers = list()
+
+for f in flowers:
+	large_flowers.append('a large ' + f)
+
+# print(large_flowers)
+
+family = { 'mother': 'Margaret', 'father': 'Reginald', 'sister': 'Jenny'}
+
+# for k in family: 
+	# print(k)
+
+# for k, v in family.items():	
+# 	print(k, v)
+
+cars = { 'Lexus', 'Chevy', 'Jeep' }
+for c in cars:
+	print(c)
